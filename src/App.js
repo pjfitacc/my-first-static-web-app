ab4352238cf6cd528aa43f764ee1e6cbe67418ff
pjfitacc <pjfitacc@gmail.com>
2025-06-04 14:39:29 -0400

    feat: add API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      // fetch the body text from the API endpoint
+      const response = await fetch('/api/message');
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const text = await response.text();
+      setData(text);
+    })();
+  });
+
+  return <div>Server's Message: {data}</div>;
 }
 
 export default App;
